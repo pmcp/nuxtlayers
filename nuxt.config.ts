927c2685c52e863e06816a0b17fe7ae74f013def
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-06-30 22:12:35 +0200

    file directory issue (https://nuxt.com/docs/guide/going-further/layers#relative-paths-and-aliases)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+
+const currentDir = dirname(fileURLToPath(import.meta.url))
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/image'],
-  css: ['~/assets/css/main.css']
+
+  css: [join(currentDir, './assets/css/main.css')]
 })
