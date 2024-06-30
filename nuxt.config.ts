6f41e49ffce266dc7d3d1597341a7abf5f14e280
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-06-30 22:16:29 +0200

    forgot to put UI in config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ const currentDir = dirname(fileURLToPath(import.meta.url))
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/image'],
+  modules: ['@nuxt/image', '@nuxt/ui'],
 
   css: [join(currentDir, './assets/css/main.css')]
 })
